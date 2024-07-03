00d72cee1b37a6783797b78a257fad50560257ee
mohamed hassan <hh71728@gmail.com>
2024-07-03 17:13:27 +0100

    0.18.1.3

--- app/src/main/java/com/example/Inventory/HomeScreen.kt
+++ app/src/main/java/com/example/Inventory/HomeScreen.kt
@@ -1,15 +1,21 @@
 package com.example.Inventory
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -17,12 +23,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.example.Inventory.navigation.NavigationDestination
 import com.example.Layer.Item
 
@@ -78,10 +89,21 @@ fun InventoryList(
     }
 }
 
+
+
 @Composable
 fun InventoryItem(
     item: Item, modifier: Modifier = Modifier
-) {
+) {   /**move state to Homeviewmodel**/
+    var selectedImages by remember {
+    mutableStateOf<Uri?>(null)
+}
+
+
+    val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { selectedImages=it }
+    )
     Card(
         modifier = modifier, elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
@@ -95,7 +117,9 @@ fun InventoryItem(
                 Text(
                     text = item.name,
                     style = MaterialTheme.typography.titleLarge,
+
                 )
+
                 Spacer(Modifier.weight(1f))
                 Text(
                     text = item.formatePrice(),
@@ -103,9 +127,24 @@ fun InventoryItem(
                 )
             }
             Text(
-                text = "in_stock, ${item.quantity}",
+                text = "in stock, ${item.quantity}",
                 style = MaterialTheme.typography.titleMedium
             )
+            /**image picker is from here->**/
+            Button(modifier=Modifier.padding(start = 190.dp),onClick = {
+                singlePhotoPickerLauncher.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                )
+            }) {
+                Text("pick image")
+            }
+            AsyncImage(
+                model = selectedImages,
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Fit
+            )
+            /**<-to here**/
         }
     }
 }
--- app/src/main/java/com/example/main_app_store/MainActivity.kt
+++ app/src/main/java/com/example/main_app_store/MainActivity.kt
@@ -3,16 +3,17 @@ package com.example.main_app_store
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.Graphs.Analytics
-import com.example.Human_Resource.Employee
+import androidx.navigation.compose.rememberNavController
+
+
+import com.example.Inventory.navigation.InventoryNavHost
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            Analytics()
+            InventoryNavHost( navController= rememberNavController())
 
         }
     }
